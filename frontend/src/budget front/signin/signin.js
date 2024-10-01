ffc6a023059ef1f3f096f64774cff130d596aeba
vlonk <135393649+vlonk@users.noreply.github.com>
2024-10-01 01:31:32 -0500

    Add files via upload

---
+++ frontend/src/budget front/signin/signin.js
@@ -0,0 +1,13 @@
+import React, { useState } from 'react';
+import { BrowserRouter as Router, useNavigate } from 'react-router-dom';
+import './signin.css';
+
+function BudgetSignIn() {
+    return(
+        <div className="content">
+            <h1>Sign in?</h1>
+        </div>
+    )
+}
+
+export default BudgetSignIn;
